df99fee75d7a30bcb5812e0b4903369b4f43714b
sarang joshi <sarangjoshi196@gmail.com>
2024-09-20 01:42:22 +0530

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const yargs = require('yargs');
-const {showEndPoints, showRoutingFiles} = require('./utils.js')
+const { showEndPoints, showRoutingFiles } = require('./utils.js')
 
 
 const argv = yargs
@@ -7,23 +7,22 @@ const argv = yargs
     .options('m', { alias: 'method', describe: 'Filter end point by method', type: 'string', demandOption: null })
     .options('w', { alias: 'middleware', describe: 'Filter end point by middleware', type: 'string', demandOption: null })
     .options('r', { alias: 'routingFiles', describe: 'show routing files', type: 'boolean', demandOption: false })
-    .options('d', { alias: 'detailedRoutingFiles', describe: 'show detailed routing file', type: 'boolean', demandOption: false })
     .help(true)
     .argv;
 
 
-    
 
-if (argv._.length) {
-    const relativeAppFilePath = yargs.argv._[0];
+if(argv.routingFiles){
+    showRoutingFiles(argv.routingFiles);
+}else{
+    if (!argv._.length) throw 'Missing app file path';
 
-    (async () => {
-        if (yargs.argv.routingFiles || yargs.argv.detailedRoutingFiles) await showRoutingFiles(yargs.argv.detailedRoutingFiles);
+    const relativeAppFilePath = argv._[0]; 
 
-        showEndPoints(relativeAppFilePath, yargs.argv);
-    })()
-    
-} else {
-    throw 'missing app file path';
+    showEndPoints(relativeAppFilePath, argv);
+
 }
+    
+
+
 
--- utils.js
+++ utils.js
@@ -2,65 +2,59 @@ const fs = require('fs');
 const _ = require('lodash');
 const { analyzeDependency } = require('express-router-dependency-graph')
 const expressListEndpoints = require('express-list-endpoints');
-const appRootFilePath = require('app-root-path');
 const pathPackage = require('path');
 
 
-const showRoutingFiles = async (detailedRoutingFiles = false) => {
-    // it's return us a array of object where each object contain route file of root directory and their routes. {filePath , routers:[]}
-    let result = await analyzeDependency({
-        outputFormat: 'json',
-        rootDir: appRootFilePath.path,
-        rootBaseUrl: '',
-        includeOnly: '',
-        doNotFollow: ['^node_modules']
-    })
-    //here we finding the max file length where route exists, for add that length when we display them into table.
-    const maxRouteFileLength = Math.max(...(result.map(e => e.filePath.length))) - appRootFilePath.path.length
-    console.table(
-        //converting the absolute path into relative path for printing. absoulte path given by analyzeDependency.
-        result.map(e => {
-            return {
-                routing_files: e.filePath.replace(appRootFilePath.path, '').padEnd(maxRouteFileLength)
-            };
+
+const showRoutingFiles = async (routingFile) => {
+    if (routingFile) {
+        const result = await analyzeDependency({
+            outputFormat: 'json',
+            rootDir: process.cwd(),
+            rootBaseUrl: '',
+            includeOnly: '',
+            doNotFollow: ['^node_modules']
         })
-    );
 
+        const routingFiles = result.map(e => {
+            return { routingFile: e.filePath.replace(process.cwd(), '') }
+        })
 
-    const pathPerRouteFile = detailedRoutingFiles ? result :
-        result.filter(
-            e => e.routers.find(f => f.method === 'use')
-        ).map(e => {
-            return { ...e, routers: e.routers.filter(f => f.method === 'use') }
-        });
+        console.table(routingFiles)
+        
+        const onlyRoutingFiles = result.map(e=>{
+            return {filePath: e.filePath, routers: e.routers.filter(f=> f.method == 'use')}
+        })
+        
+        onlyRoutingFiles.filter(e=>{
+            if(e.routers.length){
+                console.log(e.filePath.replace(process.cwd(),''))
+                console.table(e.routers,['path','method','middlewares'])
+            }
+        })
+        
 
-    // console.log(pathPerRouteFile)
-
-    pathPerRouteFile.forEach(e => {
-        console.log(e.filePath.replace(appRootFilePath, ''))
-
-        const fileRoutes = _.orderBy(e.routers, 'path');
-        const maxPathLengthFile = Math.max(...(fileRoutes.map(e => e.path.length)))
-        console.table(fileRoutes.map(e => { return { ...e, path: e.path.padEnd(maxPathLengthFile) } })
-            , ['path', 'method', 'middlewares'])
-    })
-
-
+    }
 }
 
 
 
 
 
-const showEndPoints = (relativePath, { path, method, middleware }) => {
+const showEndPoints = (relativeFilePath,  argv) => {
+    const { path, method, middleware } = argv;
+ 
 
-    const appPath = pathPackage.join(appRootFilePath.path, relativePath);
+    const appPath = pathPackage.join(process.cwd(), relativeFilePath);
 
 
     if (!fs.existsSync(appPath)) throw `${appPath} is not exists in this directory`;
 
     const RouteFile = require(appPath);
+
     let endPoints = expressListEndpoints(RouteFile);
+
+    // if any filter condition given that will filtered here.
     if (path) endPoints = endPoints.filter(e => e.path.includes(path));
     if (method) endPoints = endPoints.filter(e => e.methods.includes(method));
     if (middleware) endPoints = endPoints.filter(e => e.middlewares.includes(middleware));
